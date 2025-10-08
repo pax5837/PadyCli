b4065c3d266bfd3a107db77c2a604ce0e8d38c07
patrick5837 <patrick5837@gmail.com>
2025-10-08 21:02:02 +0200

    add basic json text sanitizer

---
+++ PadyCli.ConsoleApp/Features/JsonSanitizer/JsonSanitizerOptions.cs
@@ -0,0 +1,8 @@
+using CommandLine;
+
+namespace PadyCli.ConsoleApp.Features.JsonSanitizer;
+
+[Verb("jsani", HelpText = "Sanitizes json test from the clipboard.")]
+public class JsonSanitizerOptions
+{
+}
---
+++ PadyCli.ConsoleApp/Features/JsonSanitizer/JsonSanitizerService.cs
@@ -0,0 +1,20 @@
+using TextCopy;
+
+namespace PadyCli.ConsoleApp.Features.JsonSanitizer;
+
+internal class JsonSanitizerService
+{
+    public int Run(JsonSanitizerOptions options)
+    {
+        var text = ClipboardService.GetText();
+        var sanitizedText = text
+                .Replace("\"{", "{")
+                .Replace("}\"", "}")
+                .Replace("\\\"", "\"")
+            ;
+
+        ClipboardService.SetText(sanitizedText);
+
+        return 0;
+    }
+}
